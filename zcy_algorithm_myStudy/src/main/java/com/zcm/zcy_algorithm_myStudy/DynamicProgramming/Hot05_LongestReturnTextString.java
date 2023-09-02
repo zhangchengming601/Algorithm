bcf69351713b57c8ce2a4dfc584c3264777be18c
zhangchengming601 <2419792150@qq.com>
2023-09-02 18:44:44 +0800

    [最长回文子串]

--- zcy_algorithm_myStudy/src/main/java/com/zcm/zcy_algorithm_myStudy/DynamicProgramming/Hot05_LongestReturnTextString.java
+++ zcy_algorithm_myStudy/src/main/java/com/zcm/zcy_algorithm_myStudy/DynamicProgramming/Hot05_LongestReturnTextString.java
@@ -7,7 +7,10 @@ package com.zcm.zcy_algorithm_myStudy.DynamicProgramming;
  * */
 public class Hot05_LongestReturnTextString {
 
-    public int getLongestPalindrome (String s) {
+    /**
+     * 回文子串
+     * */
+    public int getLongestPalindrome01 (String s) {
         char[] chars = s.toCharArray();
         int res = 0;
         boolean[][] dp = new boolean[chars.length][chars.length];
@@ -30,4 +33,32 @@ public class Hot05_LongestReturnTextString {
         }
         return res;
     }
+
+    /**
+     * 最长回文子串
+     * */
+    public int getLongestPalindrome02 (String s) {
+        char[] chars = s.toCharArray();
+        int res = 0;
+        int[][] dp = new int[chars.length][chars.length];
+
+        for(int row=chars.length-1;row>=0;row--){
+            for (int col=row; col<chars.length;col++){
+                if (row==col){
+                    dp[row][col] = 1;
+                    res = Math.max(res,dp[row][col]);
+                } else if(col>row && col-row==1 && chars[row]==chars[col]){
+                    dp[row][col] = 2;
+                    res = Math.max(res,dp[row][col]);
+                }else if(col>row && col-row>1 && chars[row]==chars[col] ){
+                    if (dp[row+1][col-1]>0){
+                        dp[row][col]+=dp[row+1][col-1]+2;
+                        res = Math.max(res,dp[row][col]);
+                    }
+                }
+            }
+        }
+        return res;
+    }
+
 }
