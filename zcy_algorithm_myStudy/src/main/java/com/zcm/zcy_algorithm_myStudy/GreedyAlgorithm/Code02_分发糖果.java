6a8931a7fe3c14f43a78741eef821b97f5dd4fe9
zhangchengming601 <2419792150@qq.com>
2023-09-04 21:18:14 +0800

    [分发糖果]

---
+++ zcy_algorithm_myStudy/src/main/java/com/zcm/zcy_algorithm_myStudy/GreedyAlgorithm/Code02_分发糖果.java
@@ -0,0 +1,37 @@
+package com.zcm.zcy_algorithm_myStudy.GreedyAlgorithm;
+
+public class Code02_分发糖果 {
+
+    public int candy(int[] ratings) {
+
+        int[] num = new int[ratings.length];
+        num[0]=1;
+
+        // 先从左向右，比较右节点比左节点大的情况
+        for (int i=1;i<ratings.length;i++){
+
+            if (ratings[i]>ratings[i-1]){
+                // 如果右节点比左节点大，则右节点的得分比左节点大一个
+                num[i]=num[i-1]+1;
+            }else {
+                // 如果右节点小于等于左节点，则右节点得1分
+                num[i]=1;
+            }
+        }
+
+        // 再从右向左比较左节点比右节点大的情况
+        for (int i=ratings.length-2;i>=0;i--){
+
+            if (ratings[i]>ratings[i+1] && num[i]<num[i+1]){
+                // 如果左节点大于右节点，并且左节点的得分小于右节点，则左节点的得分比右节点大一个
+                num[i]=num[i+1]+1;
+            }
+        }
+
+        int res = 0;
+        for(int i=0;i<num.length;i++){
+            res+=num[i];
+        }
+        return res;
+    }
+}
