5a62637bb631b3a4a3be9ca4382106b9d2908a2d
zhangchengming601 <2419792150@qq.com>
2023-09-05 20:35:27 +0800

    [数组中只出现一次的两个数字]

---
+++ zcy_algorithm_myStudy/src/main/java/com/zcm/zcy_algorithm_myStudy/HashTable/Hot_03_数组中只出现一次的两个数字.java
@@ -0,0 +1,36 @@
+package com.zcm.zcy_algorithm_myStudy.HashTable;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Hot_03_数组中只出现一次的两个数字 {
+
+    public int[] FindNumsAppearOnce (int[] nums) {
+
+        int[] res = new int[2];
+        int count=0;
+
+        HashMap<Integer, Boolean> map = new HashMap<>();
+
+
+        for (int i=1;i<nums.length;i++){
+            if (map.containsKey(nums[i])){
+                map.remove(nums[i]);
+                continue;
+            }
+            map.put(nums[i],true);
+        }
+
+        Iterator<Map.Entry<Integer, Boolean>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()){
+            Map.Entry<Integer, Boolean> entry = iterator.next();
+            res[count++] = entry.getKey();
+        }
+        Arrays.sort(res);
+
+        return res;
+    }
+}
